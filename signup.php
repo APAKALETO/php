b1843c277147c345387e66f011953b618b788ae2
KISHORE KUMAR S <99709360+APAKALETO@users.noreply.github.com>
2024-03-31 09:10:59 +0530

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,26 @@
+<?php 
+ 
+$uname = $_POST['username'];
+$email = $_POST['email'];
+$pass = $_POST['password']
+
+$conn=mysql_connect("localhost","root","Apakaleto@123");
+if(!$conn)
+{
+ die('erro in connection'.mysql_error());
+}
+
+if(mysql_querry("CREATE DATABASE signup",$conn))
+{
+print "DATAbase created"
+}
+else{
+ print "database not created"
+}
+mysql_select-db("signup",$conn);
+$querry=CREATE TABLE page(username varchar(20),email varchar(20),password varchar(20))
+$querry_1=INSERT INTO page(username,email,password) VALUES($uname,$email,$pass)
+mysql_querry($querry,$conn);
+mysql_querry($querry_1,$conn);
+mysql_close($conn);
+?>
